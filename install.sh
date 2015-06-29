24e872f731bd9ac5749c329051c2b95423a4e3c4
Manuel Stofer <manuelstofer@gmail.com>
2015-06-29 15:00:51 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+if [ "$#" -ne 1 ]; then
+    echo "usage: $0 install-path"
+    echo "e.g: $0 /usr/local/bin"
+    exit 1
+fi
+
+INSTALL_PATH=$1
+
+if [ -z "$REGISSHTRY_REMOTE" ]; then
+  echo "\$REGISSHTRY_REMOTE not set";
+  exit 1
+fi
+
+cat regisshtry-receive | ssh $REGISSHTRY_REMOTE "cat > ${INSTALL_PATH}/regisshtry-receive"
+
+cp regisshtry-pull ${INSTALL_PATH}
+cp regisshtry-push ${INSTALL_PATH}
